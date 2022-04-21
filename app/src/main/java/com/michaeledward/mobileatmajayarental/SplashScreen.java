453d7eb60efbbe2f17455bf57a4edc0fbfb16fc9
user <michedwards0@gmail.com>
2022-04-21 19:36:40 +0700

    first commit

---
+++ app/src/main/java/com/michaeledward/mobileatmajayarental/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.michaeledward.mobileatmajayarental;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.michaeledward.mobileatmajayarental.LoginActivity;
+import com.michaeledward.mobileatmajayarental.R;
+
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //menghilangkan ActionBar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash_screen);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                finish();
+            }
+        }, 3000L); //3000 L = 3 detik
+    }
+}
